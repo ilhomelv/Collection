ede60756234fd94e76ae66d0bc8c4e1f2c699b69
Ilhom <34709366+ilhomelv@users.noreply.github.com>
2018-01-05 16:56:44 +0500

    Create Collection_VS_Collections.java

---
+++ Collection_VS_Collections.java
@@ -0,0 +1,16 @@
+/*
+Collection VS Colllections
+Collection Interface - Used when we to represent a group of single entity
+Defines most commonly required methods
+add(Object o)       add a particular object
+addAll(Collection c)    adds all objects to a Collection
+remove(Object o)    removes a particular object fromcollection
+removeAll(Collection c)     remove a group of objects from collecton
+clear()         clears - removes all objects from the collection
+retainAll(collection c)   except this group of Object, all the remaining objects will be removed
+
+
+
+
+
+*/
