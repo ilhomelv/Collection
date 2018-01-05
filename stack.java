e3573e00946be7f5c5313f9c475ed08d7c9f98a7
Ilhom <34709366+ilhomelv@users.noreply.github.com>
2018-01-06 00:08:07 +0500

    Create stack.java

---
+++ stack.java
@@ -0,0 +1,25 @@
+/*
+STACK- it is specially designed class which outputs:  Last in First Out order. 
+
+Stack s = new Stack();	//one constructor
+
+STACK methods:
+Object push(Object o)		insert an object into the stack
+Object pop()			    removes and returns the top of the stack (last inserted)
+Object peek()			    without removing return the top of the stack
+boolean empty()		    returns true if stack is empty
+int search(Object o)		returns offset index number if the element is available otherwise -1
+
+example:
+Stack s = new Stack();
+s.push(“A”);
+s.push(“B”);
+s.push(“C’);
+S.o.u.t (s);		//[A,B,C]		rule:  Last input, First Goes Out	
+S.o.u.t(s.search(“A”);	//3	
+S.o,u,(s.search(“Z”);	//-1
+
+	
+
+
+*/
