e84574dbaa414087577128fe3e643de19fe401f7
Ilhom <34709366+ilhomelv@users.noreply.github.com>
2018-01-05 12:59:40 +0500

    Update collectionsIntro.java

--- collectionsIntro.java
+++ collectionsIntro.java
@@ -1,10 +1,6 @@
 /*
-*    COLLECTIONS: - growable, we can increase or decrease the size
-*	          - Col. can hold both homogeneous and heterogeneous elements
-*		        - every Col. class is implemented based on some standard data structure, 
-*           hence for every requirement redeemed method support is available. being 
-*           a programmer, we re responsible to use those methods. we re not responsible 
-*           to implement those methods.
-*  
-*
+    COLLECTIONS: - growable, we can increase or decrease the size
+	        	- Col. can hold both homogeneous and heterogeneous elements
+	        	- every Col. class is implemented based on some standard data structure, hence 			for every requirement redeemed method support is available. being a 				programmer, we re responsible to use those methods. we re not responsible to 			implement those methods.
+	        	- Collections can hold objects only
 */
