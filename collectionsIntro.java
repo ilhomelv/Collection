a7593b9626cb119d6bf925e705af2d6fc42c922e
Ilhom <34709366+ilhomelv@users.noreply.github.com>
2018-01-05 12:52:08 +0500

    Create collectionsIntro.java

---
+++ collectionsIntro.java
@@ -0,0 +1,10 @@
+/*
+*    COLLECTIONS: - growable, we can increase or decrease the size
+*	          - Col. can hold both homogeneous and heterogeneous elements
+*		        - every Col. class is implemented based on some standard data structure, 
+*           hence for every requirement redeemed method support is available. being 
+*           a programmer, we re responsible to use those methods. we re not responsible 
+*           to implement those methods.
+*  
+*
+*/
