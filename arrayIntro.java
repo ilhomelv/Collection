06264ed0536749bb6fe9df13af8a3b0e8deddab0
Ilhom <34709366+ilhomelv@users.noreply.github.com>
2018-01-05 12:58:07 +0500

    Update arrayIntro.java

--- arrayIntro.java
+++ arrayIntro.java
@@ -1,7 +1,11 @@
+
 /*
-*ARRAYS - arrays are fixed in size, we can not change the size once array is created.
-*	   - arrays can hold homogeneous object, Student [ ] s can only contain student objs
-*	   - Object type array can hold any type of object, Object [ ] obj
-*	   - array is indexed and fixed collection homogeneous elements.
-*    -	To overcome problems/limitations of Arrays we go to Collections
-*/
+ARRAYS - arrays are fixed in size, we can not change the size once array is created.
+	- arrays can hold homogeneous object, Student [ ] s can only contain student objs
+	- Object type array can hold any type of object, Object [ ] obj
+	- array is indexed and fixed collection homogeneous elements. 
+	- Arrays can hold primitives and objects
+	To overcome problems/limitations of Arrays we go to Collections
+  */
+
+
