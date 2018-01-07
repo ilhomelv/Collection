79af89445b3d40853989b13a921f5d9396c66acd
Ilhom <34709366+ilhomelv@users.noreply.github.com>
2018-01-07 19:43:17 +0500

    Update queue.java

--- queue.java
+++ queue.java
@@ -1,14 +1,14 @@
 /*
 Queue Interface is a Child of Collection, Used when email sending, sms sending etc
-			From 1.5 LinkedList also implements Queue interface, LinkedList
-      implementation follows First In First Out order.
+		From 1.5 LinkedList also implements Queue interface, LinkedList
+		implementation follows First In First Out order.
 
 Queue Interface Methods
 Boolean offer(Object o)	adds object into Queue	
-Object poll()			removes n returns head element, returns Null on empty Queue
+Object poll()		removes n returns head element, returns Null on empty Queue
 Object remove()		removes n returns head element, returns NPE on empty Queue
-Object peek()			returns head element
-Object element()		returns head element
+Object peek()		returns head element
+Object element()	returns head element
 
 
 
